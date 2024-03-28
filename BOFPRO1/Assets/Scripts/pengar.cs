61e17836aefa9712eea9a51903db5f54a9983b11
Bogdan-code <gaminghavet@hotmail.com>
2024-03-28 13:27:05 +0100

    a

--- BOFPRO1/Assets/Scripts/pengar.cs
+++ BOFPRO1/Assets/Scripts/pengar.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pengar : MonoBehaviour
 {
+    static public float para = 100;
+
+    public TextMeshProUGUI paraText;
+
     // Start is called before the first frame update
-    void Start()
+    public  void RemovePara(float Cost)
     {
-        
+        para -= Cost;
+    }
+
+    public  void AddPara(float MerPara)
+    {
+        para += MerPara;
     }
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        paraText.text = "Money: "+ para.ToString();
     }
 }
