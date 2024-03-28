dc6457021272713c870fe1c53fb57ffa5fdedb6b
Bogdan-code <gaminghavet@hotmail.com>
2024-03-28 14:39:43 +0100

    Revert "Merge branch 'main' of https://github.com/Bogdan-code/BOF"
    This reverts commit 0b7f483c4b824786346ddd8fb195b171b2bf5cba, reversing
    changes made to 7cc87f064244155b7ab6f736270492ba4c8a83a9.

--- BOFPRO1/Assets/Scripts/pengar.cs
+++ BOFPRO1/Assets/Scripts/pengar.cs
@@ -1,29 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class pengar : MonoBehaviour
 {
-    static public float para = 100;
-
-    public TextMeshProUGUI paraText;
-
     // Start is called before the first frame update
-    public  void RemovePara(float Cost)
+    void Start()
     {
-        para -= Cost;
-    }
-
-    public  void AddPara(float MerPara)
-    {
-        para += MerPara;
+        
     }
 
     // Update is called once per frame
-    void Start()
+    void Update()
     {
-        paraText.text = "Money: "+ para.ToString();
+        
     }
 }
