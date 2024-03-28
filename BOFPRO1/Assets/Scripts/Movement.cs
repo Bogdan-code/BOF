a0a0b7d88aa848f975790dedd73b22c20cd54e61
Bogdan-code <gaminghavet@hotmail.com>
2024-03-28 12:47:08 +0100

    7

--- BOFPRO1/Assets/Scripts/Movement.cs
+++ BOFPRO1/Assets/Scripts/Movement.cs
@@ -6,25 +6,29 @@ using UnityEngine.Rendering;
 public class Movement : MonoBehaviour
 {
     public float speed;
+    float speedX, speedY;
+    Rigidbody2D rb;
     private Vector3 direction;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         // vi vill: få input från spelaren och sen applicera movement till spriten
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        speedX = Input.GetAxisRaw("Horizontal") * speed;
+        speedY = Input.GetAxisRaw("Vertical") * speed;
 
         // vi måste lagra variablerna horizontal och vertical i en vektor för att man ska kunna gå horizontellt och vertikalt samtidigt
 
-        Vector3 direction = new Vector3(horizontal, vertical);
+        rb.velocity = new Vector2 (speedX, speedY);
 
         // nu har vi en riktning, nu måste vi bara lägga till den till vår nuvarande position, detta gör vi i en fixedupdate eftersom vi först vill att programmet ska leta räkna ut collitions före spelaren rör sig 
 
     }
 
-    private void FixedUpdate()
-    {
-        transform.position += direction * speed * Time.deltaTime;
-    }
 
 }
