68abce1ba989101e4a4dd79058bfdc8f54eceefb
Bogdan-code <gaminghavet@hotmail.com>
2024-03-28 21:42:36 +0100

    a

--- BOFPRO1/Assets/Scripts/Movement.cs
+++ BOFPRO1/Assets/Scripts/Movement.cs
@@ -6,22 +6,8 @@ using UnityEngine.Rendering;
 public class Movement : MonoBehaviour
 {
     public float speed;
-<<<<<<< HEAD
-    private Vector3 direction;
-
-    private void Update()
-    {
-        // vi vill: få input från spelaren och sen applicera movement till spriten
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        // vi måste lagra variablerna horizontal och vertical i en vektor för att man ska kunna gå horizontellt och vertikalt samtidigt
-
-        Vector3 direction = new Vector3(horizontal, vertical);
-=======
     float speedX, speedY;
     Rigidbody2D rb;
-    private Vector3 direction;
 
 
     private void Start()
@@ -29,7 +15,7 @@ public class Movement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         // vi vill: få input från spelaren och sen applicera movement till spriten
         speedX = Input.GetAxisRaw("Horizontal") * speed;
@@ -38,18 +24,8 @@ public class Movement : MonoBehaviour
         // vi måste lagra variablerna horizontal och vertical i en vektor för att man ska kunna gå horizontellt och vertikalt samtidigt
 
         rb.velocity = new Vector2 (speedX, speedY);
->>>>>>> parent of 87145ef (Merge branch 'main' of https://github.com/Bogdan-code/BOF)
-
-        // nu har vi en riktning, nu måste vi bara lägga till den till vår nuvarande position, detta gör vi i en fixedupdate eftersom vi först vill att programmet ska leta räkna ut collitions före spelaren rör sig 
 
     }
 
-<<<<<<< HEAD
-    private void FixedUpdate()
-    {
-        transform.position += direction * speed * Time.deltaTime;
-    }
-=======
->>>>>>> parent of 87145ef (Merge branch 'main' of https://github.com/Bogdan-code/BOF)
 
 }
